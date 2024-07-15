26c07f8ae531673647cdcd011dca9b64296b197b
“ParkChangHee” <arrans9591@gmail.com>
2024-07-15 21:07:12 +0900

    구조 및 틀 짜보기

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,4 +1,5 @@
 import ImageSlider from '../ImageSlider/ImageSlider';
+import * as S from './main.styles';
 
 const MainPage = () => {
   return (
@@ -6,11 +7,46 @@ const MainPage = () => {
       <div>
         <ImageSlider />
       </div>
-      <div>EasyToUse 누구나 쉽게 가입없이 웹 프로그램을 이용하는 사이트입니다.쉽고 빠르게 사용하는 것들을 소개합니다.</div>
+      <S.Container>
+        <S.Section>
+          <S.Heading>EasyToUse의 목표</S.Heading>
+          <S.CriteriaContainer>
+            <S.Criterion>
+              <S.Checkmark>✔</S.Checkmark> 누구나 쉽게 사용할 수 있게하기
+            </S.Criterion>
+            <S.Criterion>
+              <S.Checkmark>✔</S.Checkmark> 별도의 가입 없이 사용하기
+            </S.Criterion>
+            <S.Criterion>
+              <S.Checkmark>✔</S.Checkmark> 인터넷에서 쉽게 사용하기
+            </S.Criterion>
+          </S.CriteriaContainer>
+        </S.Section>
+        <S.Section>
+          {/* <S.Heading>헤더</S.Heading> */}
+          <S.StepContainer>
+            <S.Step>
+              <S.StepNumber>EasyToUse의 첫번째 프로젝트</S.StepNumber>
+              <S.StepDescription>
+                썸네일 제작기
+                <p>누구나 쉽게 썸네일을 제작할 수 있게 만들기</p>
+              </S.StepDescription>
+            </S.Step>
+            <S.Step>
+              <S.StepNumber>EasyToUse의 첫번째 프로젝트</S.StepNumber>
+              <S.StepDescription>
+                아이디어를 구상중
+                <p>새로운 아이디어를 구상중입니다.</p>
+              </S.StepDescription>
+            </S.Step>
+          </S.StepContainer>
+        </S.Section>
+      </S.Container>
+      {/* <div>EasyToUse 누구나 쉽게 가입없이 웹 프로그램을 이용하는 사이트입니다.쉽고 빠르게 사용하는 것들을 소개합니다.</div>
       <div>EasyToUse의 첫번째 프로젝트</div>
       <div>프로젝트 자세히 보러가기</div>
       <div>EasyToUse의 두번째 프로젝트</div>
-      <div>아이디어를 생각중입니다!!</div>
+      <div>아이디어를 생각중입니다!!</div> */}
     </div>
   );
 };
---
+++ src/components/Main/main.styles.ts
@@ -0,0 +1,117 @@
+// import styled from 'styled-components';
+
+// export const Container = styled.div`
+//   font-family: Arial, sans-serif;
+//   padding: 20px;
+//   max-width: 800px;
+//   margin: 0 auto;
+// `;
+
+// export const Section = styled.section`
+//   margin-bottom: 40px;
+// `;
+
+// export const Heading = styled.h2`
+//   font-size: 24px;
+//   margin-bottom: 20px;
+// `;
+
+// export const CriteriaContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 10px;
+// `;
+
+// export const Criterion = styled.div`
+//   background: #f5f5f5;
+//   padding: 10px;
+//   border-radius: 5px;
+//   display: flex;
+//   align-items: center;
+// `;
+
+// export const Checkmark = styled.span`
+//   color: green;
+//   margin-right: 10px;
+// `;
+
+// export const StepContainer = styled.div`
+//   margin-bottom: 20px;
+// `;
+
+// export const Step = styled.div`
+//   margin-bottom: 20px;
+// `;
+
+// export const StepNumber = styled.div`
+//   font-size: 18px;
+//   font-weight: bold;
+//   margin-bottom: 10px;
+// `;
+
+// export const StepDescription = styled.div`
+//   p {
+//     margin: 5px 0 0 0;
+//     line-height: 1.6;
+//   }
+// `;
+
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  font-family: Arial, sans-serif;
+  padding: 20px;
+  max-width: 800px;
+  margin: 0 auto;
+`;
+
+export const Section = styled.section`
+  margin-bottom: 40px;
+`;
+
+export const Heading = styled.h2`
+  font-size: 24px;
+  margin-bottom: 20px;
+`;
+
+export const CriteriaContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+
+export const Criterion = styled.div`
+  background: #f5f5f5;
+  padding: 10px;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  width: 30%;
+`;
+
+export const Checkmark = styled.span`
+  color: green;
+  margin-right: 10px;
+`;
+
+export const StepContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Step = styled.div`
+  margin-bottom: 20px;
+`;
+
+export const StepNumber = styled.div`
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+
+export const StepDescription = styled.div`
+  p {
+    margin: 5px 0 0 0;
+    line-height: 1.6;
+  }
+`;
