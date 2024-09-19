86be66975a301a577e7cf0cef372019655fa3495
“ParkChangHee” <arrans9591@gmail.com>
2024-09-19 18:18:41 +0900

    그냥 배포전 수정해보기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
 'use client';
 
-import MainPage from '@/components/Main/Main';
+// import MainPage from '@/components/Main/Main';
+import SideBar from '@/components/SideBar/SideBar';
+import ThumbnailEditor from '@/components/ThumbnailEditor/ThumbnailEditor';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  /* height: 100vh; Viewport의 전체 높이를 사용 */
+`;
 
 const Home = () => {
   return (
-    <div>
-      <MainPage />
-    </div>
+    <Container>
+      <ThumbnailEditor />
+      <SideBar />
+    </Container>
   );
 };
 
